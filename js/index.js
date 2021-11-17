9d7661be9c16328bfe869079c4d8060be7ecf7ef
LUIS MARIO AMOR SANCHEZ <65006606+luismario2393@users.noreply.github.com>
2021-11-17 10:09:21 -0500

    validación de formulario

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
  
 const menuBurguer = document.querySelector('#menu-burguer-icon'); 
 const navBurguer = document.querySelector('#menu-burguer'); 
+const form = document.querySelector('#form');
+const inputLink = document.querySelector('#input-link');
+const inputSubmit = document.querySelector('#input-submit');
+const containerInputLink = document.querySelector('#container-input-link');
 
 document.addEventListener('DOMContentLoaded', () => {
   menuBurguer.addEventListener('click', menuBurguerClick);
+  form.addEventListener('submit', inputSubmitClick);
 });
+
+function inputSubmitClick(e) {
+  e.preventDefault();
+
+  const alerta = document.querySelector('.text-small');
+
+  if(!alerta) {
+    const parrafoError = document.createElement('p');
+    parrafoError.classList.add('text-red', 'margin-none', 'text-small' );
+    parrafoError.textContent = 'Please add a link'
+  
+    if(inputLink.value.length === 0) {
+      inputLink.classList.add('border-red', 'text-red');
+      containerInputLink.appendChild(parrafoError);
+    } 
+    setTimeout(() => {
+      inputLink.classList.remove('border-red', 'text-red');
+      parrafoError.remove();
+    }, 3000);
+  }
+  form.reset();
+}
 
 function menuBurguerClick(e) {
   e.preventDefault();
