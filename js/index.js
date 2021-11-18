97ddacee2c4b1a8c91a76d4543d3b0e866691311
LUIS MARIO AMOR SANCHEZ <65006606+luismario2393@users.noreply.github.com>
2021-11-17 19:36:39 -0500

    Agregando funcionalidades, leyendo API, insertando datos y guardandolo en el storage

--- js/index.js
+++ js/index.js
@@ -6,8 +6,11 @@ const inputLink = document.querySelector('#input-link');
 const inputSubmit = document.querySelector('#input-submit');
 const containerInputLink = document.querySelector('#container-input-link');
 const containerInputSubmit = document.querySelector('#container-input-submit');
-
+const resultado = document.querySelector('#resultado');
+let urls = [];
 document.addEventListener('DOMContentLoaded', () => {
+  urls = JSON.parse(localStorage.getItem('urls')) || [];
+  insertarHtml(urls);
   menuBurguer.addEventListener('click', menuBurguerClick);
   form.addEventListener('submit', inputSubmitClick);
 });
@@ -15,7 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
 function inputSubmitClick(e) {
   e.preventDefault();
 
+  
   const alerta = document.querySelector('.text-small');
+  const urlsI = {
+    id: Date.now(),
+    url: inputLink.value,
+  }
 
   if(!alerta) {
     const parrafoError = document.createElement('p');
@@ -23,11 +31,15 @@ function inputSubmitClick(e) {
     parrafoError.textContent = 'Please add a link'
 
   
-    if(inputLink.value.length === 0) {
+    if(urlsI.url.length === 0) {
       inputLink.classList.add('border-red', 'text-red');
       inputSubmit.classList.add('margin-bottom');
       containerInputLink.appendChild(parrafoError);
-    } 
+    } else {
+      urls.push(urlsI);
+      consultarAPI(urlsI.url);
+      
+    }
     setTimeout(() => {
       inputLink.classList.remove('border-red', 'text-red');
       inputSubmit.classList.remove('margin-bottom');
@@ -35,6 +47,54 @@ function inputSubmitClick(e) {
     }, 3000);
   }
   form.reset();
+}
+
+function consultarAPI(urlInput) {
+  const url = `https://api.shrtco.de/v2/shorten?url=${urlInput}/very/long/link.html`;
+  const urlShort = {
+    urlShort: ''
+  }
+
+  fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          urlShort.urlShort = data.result.short_link;
+          Object.assign(urls[urls.length - 1], urlShort);
+          insertarHtml(urls);
+        })
+        .catch(error => console.log(error));
+}
+
+function insertarHtml(datos) {
+  console.log(datos)
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+  datos.forEach(dato => {
+    const {  url, urlShort } = dato;
+    
+    const urlsMax = url.slice(0, 60);
+
+    const div = document.createElement('div');
+    div.classList.add('container-resultado');
+    div.innerHTML = `
+      <p>${urlsMax}</p>
+      <div class="container-copy">
+        <a href="${url}">${urlShort}</a>
+        <div class="copy-x">
+          <input class="input-submit button-copy" type="submit" value="Copy" id="input-submit-copy">
+          <i class="far fa-times-circle icon-close" id="icon-close"></i>
+        </div>
+      </div>
+    `;
+    resultado.appendChild(div);
+  });
+
+  sincronizarStorage();
+
+}
+function sincronizarStorage() {
+  localStorage.setItem('urls', JSON.stringify(urls));
 }
 
 function menuBurguerClick(e) {
